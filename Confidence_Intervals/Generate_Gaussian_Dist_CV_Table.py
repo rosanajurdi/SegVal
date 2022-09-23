a2f0bdd0de2c2992aec6fe27ceb90327c44223d6
rosanajurdi <rosana_jurdi@live.com>
2022-09-24 00:55:15 +0200

    Fixed Table formats to SEM-Wk

--- Confidence_Intervals/Generate_Gaussian_Dist_CV_Table.py
+++ Confidence_Intervals/Generate_Gaussian_Dist_CV_Table.py
@@ -10,7 +10,7 @@ N samples between 10 and 1000.
 #sigma = [0.6,1.8,3.6,18,28.8, 36, 54,57.6,59.4]
 
 mu = 82.
-sigma = [ 0.56 ,  0.70, 0.98 , 1.4 , 2.1 , 3.5 , 4.2 , 7.7,  9.1]
+sigma = [ 2.1 , 3.5 , 4.2 ,6.97,  7.7,  9.1]
 N_samples = [10,20,30,50,100,200,300,500,1000]
 
 import numpy as np
@@ -19,8 +19,13 @@ for s in sigma:
 # for a 95 confidence interval statistic the rule is X - 2*sigma/sqrt(n)
 print('\hline')
 for n in N_samples:
-    print('n = {}'.format(n))
+    print('$k = {}$'.format(n))
     for s in sigma:
         print( "&", [np.round(mu - 1.96*s/np.sqrt(n),2), np.round(mu + 1.96*s/np.sqrt(n), 2)])
     print('\\ \ ')
 
+for k in N_samples:
+    print('$k = {}$'.format(k))
+    for s in sigma:
+        print( "&", np.round(s/np.sqrt(k), 2),'&', np.round(2*1.96*s/np.sqrt(k), 2))
+    print('\\ \ ')
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import os
 # Import the dataset
 root_path = '/Users/rosana.eljurdi/PycharmProjects/SegVal_Project/Stats/Test/fold_3/results-dice-3D.csv'
 data = pd.read_csv(root_path)
-# Create the pandas dataframe for the statistical experiments 
+# Create the pandas dataframe for the statistical experiments
+
 
 # print Analytical statistical alues:
 statistical_analysis(data)
@@ -18,6 +19,32 @@ if 'dice' in os.path.basename(root_path) :
     data['metric'] *= 100
     data['metric'] = (data['metric']).round(2)
 
+
+#Visualize the Distribution:
+
+plt.figure()
+#fig, ax = plt.subplots(2, 1)
+#bx = sns.boxplot(y = data['metric'], width=.3)
+#plt.title('BoxPlot of Hippocampus Dataset.')
+#bx.set_ylim(0, 100)
+data['Dice'] = data['metric']
+data.boxplot(column='Dice')
+plt.title('Box Plot of Hippocampus Dataset.')
+plt.figure()
+sns.distplot(data['Dice'], hist=True, kde=True,
+             bins=int(180 / 5), color='darkblue',
+             hist_kws={'edgecolor': 'black'},
+             kde_kws={'linewidth': 4})
+
+#plt.legend(prop={'size': 16}, title='Hippo-Distribution')
+plt.title('Histogram and Density Distribution of Hippocampus Dataset.')
+
+
+'''
+
+path = root_path.split(os.path.basename(root_path))
+
+plt.savefig(os.path.join(path, 'Distribution-Hippo'))
 
 K_samples = [ 10,20,30,50,100,110 ]
 save_dir = root_path.split(os.path.basename(root_path))[0]
@@ -32,22 +59,28 @@ df = pd.read_csv(root_path)
 #
 df.boxplot(column='u-kj', by='sample-set')
 plt.savefig(os.path.join(basepath, "Boxplot-Hippo-u-kj"))
-df.hist(column='u-kj', by='sample-set')
-plt.savefig(os.path.join(basepath, "Hist-Hippo-u-kj"))
+#df.hist(column='u-kj', by='sample-set')
+#plt.savefig(os.path.join(basepath, "Hist-Hippo-u-kj"))
 print("hello")
 
 df.boxplot(column='sigma-kj', by='sample-set')
 plt.savefig(os.path.join(basepath, "Boxplot-Hippo-sigma-kj"))
-df.hist(column='sigma-kj', by='sample-set')
-plt.savefig(os.path.join(basepath, "Hist-Hippo-sigma-kj"))
+#df.hist(column='sigma-kj', by='sample-set')
+#plt.savefig(os.path.join(basepath, "Hist-Hippo-sigma-kj"))
 print('hello 2')
 
 df.boxplot(column='SEM-kj', by='sample-set')
 plt.savefig(os.path.join(basepath, "Boxplot-Hippo-SEM-kj"))
-df.hist(column='SEM-kj', by='sample-set')
-plt.savefig(os.path.join(basepath, "Hist-Hippo-SEM-kj"))
+#df.hist(column='SEM-kj', by='sample-set')
+#plt.savefig(os.path.join(basepath, "Hist-Hippo-SEM-kj"))
 print('hello 2')
+'''
 
+root_path = "/Users/rosana.eljurdi/PycharmProjects/SegVal_Project/Stats/Test/fold_3/subsampled-stats-Hippo-Sep16.csv"
+df = pd.read_csv(root_path)
+df['w-k'] = 2*1.96*df['SEM-kj']
+
+K_samples = [ 10,20,30,50,100,110 ]
 for k in K_samples:
     df_sample = df[df['sample-set']== k]
     assert df_sample.__len__() == 100 # Make sure that there are only a 100 samples selected.
@@ -55,19 +88,10 @@ for k in K_samples:
     mean_variation = '{} \pm {}'.format(df_sample['u-kj'].mean().round(2), df_sample['u-kj'].std().round(2))
     std_variation = '{} \pm {}'.format(df_sample['sigma-kj'].mean().round(2), df_sample['sigma-kj'].std().round(2))
     SEM_Variation =  '{} \pm {}'.format(df_sample['SEM-kj'].mean().round(2), df_sample['SEM-kj'].std().round(2))
-    plt.figure()
-    sns.distplot(df_sample['u-kj'], hist=True, kde=True,
-                 bins=int(180 / 5), color='darkblue',
-                 hist_kws={'edgecolor': 'black'},
-                 kde_kws={'linewidth': 4})
-
-    plt.legend(prop={'size': 16}, title='Hippo-{}-u-kj'.format(N))
-    plt.title('Histogram and Density Distribution of Mean Dice of sample N = {}'.format(N))
+    Width_Variation = '{} \pm {}'.format(df_sample['w-k'].mean().round(2), df_sample['w-k'].std().round(2))
 
 
-    plt.savefig(os.path.join(basepath, 'Distribution-Hippo-{}'.format(N)))
-
-    print('k = ' + str(N) + "&" +mean_variation + "&" + std_variation + "&" + SEM_Variation)
+    print('k = ' + str(k) + "&" +mean_variation + "&" + std_variation + "&" + SEM_Variation + "&" + Width_Variation)
 
 
 
